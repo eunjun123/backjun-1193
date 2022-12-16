a110d45034daf95a7accf51d91ec88b9bb609b67
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 18:47:55 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string X = Console.ReadLine();
+        int x = int.Parse(X);
+
+        int lineCount = 1;
+
+        while (x > lineCount)
+        {
+            x -= lineCount;
+            lineCount++;
+        }
+
+        if (lineCount % 2 == 1)
+            Console.Write($"{lineCount - x + 1}/{x}");
+        else
+            Console.Write($"{x}/{lineCount - x + 1}");
+    }
+}
